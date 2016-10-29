152b8f9307ca39606025721d71db0677a285f091
ABigBigWhale <chakalakafrank@gmail.com>
2016-10-28 21:24:55 -0700

    Added extra feature: like/unlike

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Initialized empty README
+# Hao Liu
+## INFO 343, Assignment 4
+
+### What is the URL of this project on your student web-server?
+http://students.washington.edu/liuh25/info343/a4-firebase/
+
+### Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+No. I did the assignment individually.
+
+### Approximately how many hours did it take you to complete this challenge?
+10 hours
+
+### Did you encounter any specific problems in this challenge we should warn students about in the future? How can we make the challenge better?
+
+### Extra features
+1. Order by newest/hottest. Toggle buttons on top in nav bar.
+2. Each user can only like ONCE, and clicking on the like icon will toggle like/un-like. Changes are reflected in Firebase, which then generates interface outputs.
+3. Integrated toggling Sign-in/Sign-up interface. Errors presented on page instead of as alerts.
+4. Database stores username & self username display.
+5. Animations for loading tweets.
--- js/main.js
+++ js/main.js
@@ -39,6 +39,9 @@ $(document).ready(function() {
         var momentUserPhoto = $('<img>', {class: 'profile-photo', src: data.photoURL});
         var momentText = $('<div>', {class: 'moment-text'}).append($('<p>', {text: data.momentText}));
         var momentLikeCount = $('<span>', {class: 'like-count', text: data.likeCount});
+        var momentLikeIcon = currentUserLiked(data) ? 'fa-heart' : 'fa-heart-o';
+
+
 
         var momentCard = $('<div>', {class: 'row'}).append(
             $('<div>', {class: 'card moment-card', id: id}).append(
@@ -51,7 +54,7 @@ $(document).ready(function() {
                 )
             ).append(
                 $('<div>', {class: 'card-action'}).append(
-                    $('<i>', {class: 'fa fa-heart-o clickable', 'aria-hidden': 'true'})
+                    $('<i>', {class: 'fa ' + momentLikeIcon + ' clickable', 'aria-hidden': 'true'})
                 ).append(
                     momentLikeCount
                 )
@@ -84,14 +87,14 @@ $(document).ready(function() {
                 moment.momentId = key;
                 return moment;
             });
-            
+
             console.log(allMoments);
 
             $('#moments-container').empty();
             if (view == 'view-hottest') {
                 allMoments.sort( (m1, m2) => parseInt(m1.likeCount) - parseInt(m2.likeCount) );
             }
-            
+
             allMoments.forEach(moment => {
                 var momentId = moment.momentId;
                 delete moment.momentId;
@@ -100,9 +103,31 @@ $(document).ready(function() {
         });
     };
 
+    var currentUserLiked = function(moment) {
+        
+        if ('likedUsers' in moment) {
+            
+            for (var key in moment.likedUsers) {
+                if (moment.likedUsers[key] == user.uid) {
+                    return key;
+                }
+            }
+
+        }
+        
+        return null;
+    }
+
     $('#submit-new-moment').click(function(){
         console.log("new moment!")
         $('#form-new-moment').submit();
+    });
+    
+    $("#new-moment").keypress(function (e) {
+        if(e.which == 13) {
+            e.preventDefault();
+            if ($(this).val()) $('#form-new-moment').submit();
+        }
     });
 
     $('#form-new-moment').on('submit', function(event){
@@ -111,32 +136,48 @@ $(document).ready(function() {
         moments.push({
             photoURL: user.photoURL,
             momentText: $('#new-moment').val(),
-            likeCount: 0
+            likeCount: 0,
+            likedUsers: {}
         });
 
         $('#new-moment').val('').css('height', 'auto');
     });
 
     $('#moments-container').on('click', 'i.fa-heart-o, i.fa-heart', function(){
+        var heartIcon = $(this);
         var momentId = $(this).closest('.moment-card').attr('id');
-        moments.child(momentId).once('value').then(function(snapshot){
-            console.log(snapshot.val().likeCount);
+        var moment = moments.child(momentId);
+
+        moment.once('value').then(function(snapshot){
             var prevCount = snapshot.val().likeCount;
-            moments.child(momentId).update({likeCount: prevCount + 1});
+            var userLikeKey = currentUserLiked(snapshot.val());
+            console.log(userLikeKey);
+
+            if (!userLikeKey) {
+                moment.child('likedUsers').push(user.uid);
+                moment.update({likeCount: prevCount + 1});
+                if (!heartIcon.hasClass('fa-heart')) heartIcon.addClass('fa-heart');
+                if (heartIcon.hasClass('fa-heart-o')) heartIcon.removeClass('fa-heart-o');
+            } else {
+                moment.child('likedUsers').child(userLikeKey).remove();
+                moment.update({likeCount: prevCount - 1});
+                if (!heartIcon.hasClass('fa-heart-o')) heartIcon.addClass('fa-heart-o');
+                if (heartIcon.hasClass('fa-heart')) heartIcon.removeClass('fa-heart');
+            }
+
         });
-        $(this).toggleClass('fa-heart-o').toggleClass('fa-heart');
     });
 
     $('.view-select-list').on('click', '.clickable', function(){
         var newViewType = $(this).attr('id');
         $(this).closest('.view-select').siblings().removeClass('active');
         $(this).closest('.view-select').addClass('active');
-        
+
         if (newViewType != viewType) {
             viewType = newViewType;
             reloadMoment(viewType);
         }
-        
+
         console.log(viewType);
     });
 
